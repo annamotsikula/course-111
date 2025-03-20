c36ebf612bac5f38c64bbea7b7c7ac2b1ea6a856
Anna <annamotsikula@gmail.com>
2025-03-20 18:45:46 +0400

    add: initial styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
-import { ProfileComponent } from './profile/profile.component';
-import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-import { ChildFunctionComponent } from './core/general-functions/singleFunction/child-function/child-function.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ProfileComponent,
-    UserDashboardComponent,
-    ChildFunctionComponent
   ],
   imports: [
     BrowserModule,
